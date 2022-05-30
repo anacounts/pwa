7ccd2edb647682a04e5fe067b57ac7f6071c3b56
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-05-30 13:53:56 +0200

    feat: enhance profile

---
+++ src/components/Button.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+import "./Button.css";
+
+function maybeRenderLink(to, props, children) {
+  if (!to) return <button {...props}>{children}</button>;
+
+  return (
+    <Link to={to} {...props}>
+      {children}
+    </Link>
+  );
+}
+
+function buttonColorModifier(type) {
+  return "button--color-" + type;
+}
+
+function Button({ children, className, color, to, ...props }) {
+  const colorModifier = buttonColorModifier(color);
+
+  return maybeRenderLink(
+    to,
+    { className: `button ${colorModifier} ${className ?? ""}`, ...props },
+    children
+  );
+}
+
+Button.propTypes = {
+  color: PropTypes.oneOf(["cta", "feature", "invisible"]),
+};
+
+export default Button;
