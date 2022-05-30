5694ad197a7ed07d4387b6718293e2acda455db0
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2022-05-30 15:35:45 +0200

    feat: allow `className` on Loader + cleanup

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,9 +2,22 @@ import React from "react";
 
 import "./Loader.css";
 
-function Loader({ color = "theme", size = "base" }) {
+function loaderColorModifier(color) {
+  return "loader--color-" + color;
+}
+
+function loaderSizeModifier(size) {
+  return "loader--size-" + size;
+}
+
+function Loader({ color = "theme", size = "base", className }) {
+  const colorModifier = loaderColorModifier(color);
+  const sizeModifier = loaderSizeModifier(size);
+
   return (
-    <span className={`loader loader--color-${color} loader--size-${size}`} />
+    <span
+      className={`loader ${colorModifier} ${sizeModifier} ${className ?? ""}`}
+    />
   );
 }
 
